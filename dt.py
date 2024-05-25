d58b0de5cffb7071849d6e36906d4c50c4a73506
Muhammad hassaan Khan <muhammadhassaankhan@Hassaans-MBP.bbrouter>
2024-05-25 22:25:02 +0500

    removed unnecessary stuff

--- dt.py
+++
@@ -1,5 +0,0 @@
-import datetime
-a = datetime.datetime.now()
-day=a.strftime("%a-%Y-%m")
-print("Time is ",day,"AD")
-
--- practice.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-class FlightReservation;
-int main(){
-
-return 0;
-}
